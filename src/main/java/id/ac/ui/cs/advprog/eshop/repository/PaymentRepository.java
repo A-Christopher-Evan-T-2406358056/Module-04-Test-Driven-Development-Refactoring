8b91f9c1cda87ffc5590ea7469f96f1f8dc61451
Evan <88143635+evan052006@users.noreply.github.com>
2026-03-10 20:36:52 +0700

    [GREEN] Implemented PaymentRepository.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,14 +11,24 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null;
+        for (int i = 0; i < paymentData.size(); i++) {
+            if (paymentData.get(i).getId().equals(payment.getId())) {
+                paymentData.set(i, payment);
+                return payment;
+            }
+        }
+        paymentData.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
-        return null;
+        return paymentData.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     public List<Payment> getAllPayments() {
-        return null;
+        return new ArrayList<>(paymentData);
     }
 }
