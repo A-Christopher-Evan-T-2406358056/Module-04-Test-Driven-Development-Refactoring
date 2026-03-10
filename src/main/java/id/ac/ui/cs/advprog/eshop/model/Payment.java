ee90d221d272c89b12aa043e2b5a5efca5bc16fe
Evan <88143635+evan052006@users.noreply.github.com>
2026-03-10 20:26:13 +0700

    Created class skeletons

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class Payment {
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+    private Order order;
+
+    public Payment(String paymentId, String method, Map<String, String> paymentData, Order order) {
+    }
+
+    public void setStatus(String status) {
+        return;
+    }
+}
